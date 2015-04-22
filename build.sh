931de8fa4dde51316754f571a00d0ccee9b47f55
alex.collins <alex.collins@gamesys.co.uk>
2015-04-22 11:48:12 +0100

    master

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 set -eux
 
-mvn install -DskipTests
-mvn versions:use-latest-releases -rf :docker-java-orchestrator
+# mvn clean
+# mvn install -DskipTests
+# mvn versions:use-latest-releases -rf :docker-java-orchestration -Dincludes='com.alexecollins*'
 mvn install -Prun-its
 
 cd docker-gradle-pluin
